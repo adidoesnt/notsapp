ae726a6456a6ea213b3c2fcedfb8f0844cf907ac
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-05-20 17:59:14 +0800

    add backend express boilerplate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get("/", (_, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
