fe43df89597b24d476587df73a3ef05090920363
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-05-23 15:36:34 +0800

    remove sequelize

--- backend/src/api/models/index.ts
+++
@@ -1,4 +0,0 @@
-export { User } from './user';
-export { Chat } from './chat';
-export { Message } from './message';
-export { ChatUser } from './chatUser';
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 import { routes } from 'api/router';
 import { getHttpServer, getServer } from 'api/server';
-import { database } from 'api/components/sequelize';
 const { PORT = 3001 } = process.env;
 
 const app = express();
@@ -17,6 +16,5 @@ app.use(routes());
 const httpServer = getHttpServer(app);
 const server = getServer(httpServer);
 
-await database.init();
 server.init();
 httpServer.init(PORT);
