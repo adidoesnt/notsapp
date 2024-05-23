6efaab961c0207acc874a3d1b571b5eb6e3c3b43
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-05-23 17:05:55 +0800

    add get user by uuid

--- backend/src/repositories/user.ts
+++ backend/src/repositories/user.ts
@@ -3,6 +3,7 @@ import { prisma } from 'index';
 import { v4 as uuidV4 } from 'uuid';
 
 export type CreateUserAttributes = Omit<Prisma.UserCreateInput, 'UUID'>;
+export type GetUserAttributes = Prisma.UserFindUniqueOrThrowArgs;
 
 export const createUser = async (data: CreateUserAttributes) => {
     try {
@@ -12,6 +13,20 @@ export const createUser = async (data: CreateUserAttributes) => {
         });
         return createdUser;
     } catch (error) {
-        console.error('Failed to create user');
+        console.error('Failed to create user', error);
+    }
+};
+
+export const getUserByUUID = async (UUID: string) => {
+    try {
+        const where: GetUserAttributes['where'] = {
+            UUID
+        };
+        const foundUser = await prisma.user.findUniqueOrThrow({
+            where
+        });
+        return foundUser;
+    } catch (error) {
+        console.error('Failed to find user by UUID', error);
     }
 };
